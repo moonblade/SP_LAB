2e72bccac966b847639cb6e5621d1986b52b47ba
MoonBlade <moonblade168@gmail.com>
2016-02-01 21:45:50 +0530

    twoPass

--- twopass.cpp
+++ twopass.cpp
@@ -7,31 +7,23 @@
 #include<list>
 #include<stdlib.h>
 using namespace std;
-fstream f,g;
+fstream f,g,h;
 // location counter and starting address default value
-int locctr=0,startaddress=0;
+int locctr=0,startaddress=0,programSize;
 // line number in case of errors
 int linenumber=0;
 
 // A class for each entry in symtab,
-// It consists of a label and address and a list of forward references
+// It consists of a label and address 
 class symtabentry{
     public:
     string label;
     int address;
-    list<int> fwdref;
-    // For inserting a new entry into symtab, without any previous entries
+    // For inserting a new entry into symtab
     symtabentry(string label,int address)
     {
         this->label=label;
         this->address=address;
-    }
-    // for inserting entry without value but with forward references
-    symtabentry(string label, list<int> fwdref)
-    {
-        this->label=label;
-        this->address=-1;
-        this->fwdref = fwdref;
     }
 };
 // Symtab is a list of sytabEntries
@@ -53,34 +45,10 @@ class textRecord
     string record;
     
     // Crete a new textRecord as continuation, so location counter is chosen as start
-    textRecord()
-    {
-        // need to convert location counter to hex string to create new textRecord, so string stream is required
-        stringstream s;
-        s<<setw(6)<<setfill('0')<<hex<<locctr;
-        // Start is set as T^ appended with the location counter in hex with 6 width length and filled with '0'
-        // ie if location counter is 1A, it would become 00001A
-        start = "T^" + s.str() + "^";
-        // size starts as zero, upon each insertion size is increased and during printing, it is appended in hex as second element
-        size=0;
-        // the ^ after size is inserted in record, rest of the insertions are appended in record
-        record = "^";
-    }
-    
-    // During forward reference resolution, new textrecords are to be made to insert the forward referenced correction in the correct location, so start is the given address
-    // It can be grouped as a single constructor with defualat constructor, to avoid confusion two have been made
-    /*
-    consider, the following
-    1000:            ADD  BETA                      T^001000^03^5B0000 // zero since beta is not known
-    10003:  BETA WORD 2                             T^001001^02^1003   // 1001 ie 0000 is corrected to 1003 when value of beta is known
-    so T^1001^02^2001
-    */
-    
-    textRecord(int locctr)
+    textRecord(string locctr)
     {
         stringstream s;
-        // here the locctr used is local one not global one, rest is same as earlier
-        s<<setw(6)<<setfill('0')<<hex<<locctr;
+        s<<setw(6)<<setfill('0')<<locctr;
         // Start is set as T^ appended with the location counter in hex with 6 width length and filled with '0'
         // ie if location counter is 1A, it would become 00001A
         start = "T^" + s.str() + "^";
@@ -95,7 +63,7 @@ class textRecord
     {
         // textRecord maximum size is 1E
         if(this->size+size>30)
-        return 0;
+            return 0;
         // Insert if maximum size is not violated
         this->record+=record+"^";
         this->size+=size;
@@ -124,12 +92,11 @@ list<textRecord> textRecords;
 /*
 A function to insert an entry into symtab
 Cases for symtab edit and insert value:
-1. entry does not exist, value is known (no forward reference)
-2. entry already exists without value, insert value
-3. entry already exists with value, insert another value - ERROR
+1. entry does not exist, value is known
+2. entry already exists with value, insert another value - ERROR
 here locctr is the value of the label
 */
-int insertToSymtab(string label,int locctr, textRecord &currentTextRecord)
+int insertToSymtab(string label,int locctr)
 {
     list<symtabentry>::iterator iter;
     for(iter=symtab.begin();iter!=symtab.end();++iter)
@@ -137,41 +104,10 @@ int insertToSymtab(string label,int locctr, textRecord &currentTextRecord)
         
         if((*iter).label==label)
         {
-            // Label already exists, it may or may not have a value
+            // Redeclaration of symbol, ERROR
             {
-                if((*iter).address==-1)
-                {
-                    // No declaration found, ie there is no value for it, which means there was a forward reference so make textRecords for forward references
-                    (*iter).address = locctr;
-                    if(currentTextRecord.size>0)
-                    {
-                        // print the previous textRecord
-                        textRecords.push_back(currentTextRecord);
-                        currentTextRecord = *new textRecord();
-                    }
-                    // loop through the list of forward references and make a textRecord for each of them
-                    // iterator to loop through the list
-                    list<int>::iterator iterint;
-                    stringstream s;
-                    s<<setw(4)<<setfill('0')<<hex<<locctr;
-                    
-                    for(iterint = (*iter).fwdref.begin(); iterint!=(*iter).fwdref.end();iterint++)
-                    {
-                        // Create a new text record to insert
-                        textRecord forwardReferenceTextRecord = *new textRecord(*iterint);
-                        // locctr is to be converted to 4 width string of bytesize=2 to insert into textRecord
-                        forwardReferenceTextRecord.insertRecord(s.str(),2);
-                        // insert it into the list of textRecords
-                        textRecords.push_back(forwardReferenceTextRecord);
-                    }
-                    return 1;
-                }
-                else
-                {
-                    // Redeclaration of symbol, ERROR
-                    cout<<"ERROR : REDECLARATION OF VARIABLE" + label + " - line "<<dec<<linenumber;
+                    cout<<"ERROR : REDECLARATION OF VARIABLE " + label + " - line "<<dec<<linenumber;
                     return 0;
-                }
             }
         }
     }
@@ -187,35 +123,11 @@ int getSymbolValue(string label)
     list<symtabentry>::iterator iter;
     for(iter=symtab.begin();iter!=symtab.end();++iter)
     {
-        // return address if found(can be -1)
+        // return address if found
         if((*iter).label==label)
         return (*iter).address;
     }
     return -1;
-}
-
-/*
-Cases where forard reference is used
-1. entry does not exist, value is not known (forward reference to be added)
-2. entry already exists without value, insert another forward reference
-*/
-
-void insertForwardReference(string label,int locctr)
-{
-    list<symtabentry>::iterator iter;
-    for(iter=symtab.begin();iter!=symtab.end();++iter)
-    {
-        // If label already exists, insert forward reference at end
-        if((*iter).label==label)
-        {
-            (*iter).fwdref.push_back(locctr);
-            return;
-        }
-    }
-    // No label exists, create one with -1 as address, and add fwdref to it
-    list<int> oneItemList;
-    oneItemList.push_back(locctr);
-    symtab.push_back(*new symtabentry(label,oneItemList));
 }
 
 // Search the optab and find the hex equivalent of the opcode, if not found, return null
@@ -235,11 +147,13 @@ string getHexOpcode(string opcode)
     return "null";
     
 }
-int main(int argc, char const *argv[])
+
+int passOne()
 {
     // open source and optab
     f.open("source",ios::in);
     g.open("optab",ios::in);
+    h.open("intermediary",ios::out);
     // strings required in the procedure
     string line,opHex;
     string headerRecord,endRecord;
@@ -259,9 +173,8 @@ int main(int argc, char const *argv[])
         str<<setw(6)<<setfill('0')<<hex<<operand;
         str>>locctr;
         startaddress=locctr;
-        // Create header record, without size
-        headerRecord="H^"+label+"^"+str.str()+"^"; // append size here
-        cout<<setw(4)<<setfill('0')<<hex<<locctr<<": "<<label<<" "<<opcode<<" "<<operand<<endl;
+        cout<<setw(4)<<setfill('0')<<hex<<locctr<<" "<<label<<" "<<opcode<<" "<<operand<<endl;
+        h<<setw(4)<<setfill('0')<<hex<<locctr<<" "<<label<<" "<<opcode<<" "<<operand<<endl;
         // Get next line since, first line is already processed, and split it
         getline(f,line);
         linenumber++;
@@ -269,11 +182,7 @@ int main(int argc, char const *argv[])
         getline(s,label,' ');
         getline(s,opcode,' ');
         getline(s,operand,' ');
-        
     }
-    
-    // Current textRecord to which insertion takes place
-    textRecord currentTextRecord;
     // Loop till end of file
     while(getline(f,line))
     {
@@ -281,23 +190,126 @@ int main(int argc, char const *argv[])
         // If not a comment line
         if(label[0]!='.')
         {
-            
             // insertion length
             int lengthCurrentEntry = 0; // for uniformity
-            bool breakOff = 0;
-            // record to be inserted
-            string record="";
-            // print the line
-            cout<<setw(4)<<setfill('0')<<hex<<locctr<<": "<<label<<" "<<opcode<<" "<<operand<<endl;
+            cout<<setw(4)<<setfill('0')<<hex<<locctr<<" "<<label<<" "<<opcode<<" "<<operand<<endl;
+            h<<setw(4)<<setfill('0')<<hex<<locctr<<" "<<label<<" "<<opcode<<" "<<operand<<endl;
             if(label!=""){
                 // If a label is found, immediately insert it to symtab
-                insertToSymtab(label,locctr,currentTextRecord);
+                if(!insertToSymtab(label,locctr))
+                    return 0;
             }
             // Try to get hex equivalent of opcode
             if(getHexOpcode(opcode)!="null")
             {
                 // opcode is valid
                 lengthCurrentEntry=3;
+            }
+            // opcode is assembler directive
+            else if(opcode=="WORD")
+            {
+                lengthCurrentEntry=3;
+            }
+            else if(opcode=="RESW")
+            {
+                lengthCurrentEntry=3*atoi(operand.c_str());
+            }
+            else if(opcode=="BYTE")
+            {
+                int len=operand.length()-3;
+                if(operand[0]=='X')
+                {
+                    len/=2;
+                }
+                lengthCurrentEntry=len;
+            }
+            else if(opcode=="RESB")
+            {
+                lengthCurrentEntry=atoi(operand.c_str());
+            }
+            else
+            {
+                // Invalid opcode encountered
+                cout<<"Error: INVALID OPCODE " + opcode + " line "<<dec<<linenumber;
+                return 0;
+            }
+                // inncrease locctr
+                locctr+=lengthCurrentEntry;
+        }
+        // increase line no.
+        linenumber++;
+        // split next line, which was prefetched
+        stringstream s(line);
+        getline(s,label,' ');
+        getline(s,opcode,' ');
+        getline(s,operand,' ');
+    }
+    programSize = locctr - startaddress;
+    // close files
+    f.close();
+    g.close();
+    h.close();
+    return 1;
+}
+
+int passTwo()
+{
+    // open intermediary and optab
+    f.open("intermediary",ios::in);
+    g.open("optab",ios::in);
+    // strings required in the procedure
+    string line,opHex;
+    string headerRecord,endRecord;
+    string locctrString,label,opcode,operand;
+    // get the first line, to check for start
+    getline(f,line);
+    linenumber++;
+    stringstream s(line);
+    // split the line
+    getline(s,locctrString,' ');
+    getline(s,label,' ');
+    getline(s,opcode,' ');
+    getline(s,operand,' ');
+    if(opcode=="START")
+    {
+        // if start line is defined
+        stringstream str;
+        str<<setw(6)<<setfill('0')<<locctrString;
+        // Create header record, without size
+        stringstream str2;
+        str2<<setw(6)<<setfill('0')<<hex<<programSize;
+        
+        headerRecord="H^"+label+"^"+str.str()+"^"+str2.str();
+        // Get next line since, first line is already processed, and split it
+        getline(f,line);
+        linenumber++;
+        stringstream s(line);
+        getline(s,locctrString,' ');
+        getline(s,label,' ');
+        getline(s,opcode,' ');
+        getline(s,operand,' ');
+        
+    }
+    
+    // Current textRecord to which insertion takes place
+    textRecord currentTextRecord(locctrString);
+    // Loop till end of file
+    while(getline(f,line))
+    {
+        // the next line is prefetched, but previous line is processed here
+        // If not a comment line
+        if(label[0]!='.')
+        {
+            // insertion length
+            int lengthCurrentEntry = 0; // for uniformity
+            bool breakOff = 0;
+            // record to be inserted
+            string record="";
+            // Try to get hex equivalent of opcode
+            if(getHexOpcode(opcode)!="null")
+            {
+                // opcode is valid
+                lengthCurrentEntry = 3;
                 record+=getHexOpcode(opcode);
                 // if there is an operand
                 if(operand!="")
@@ -306,9 +318,8 @@ int main(int argc, char const *argv[])
                     int value=getSymbolValue(operand);
                     if(value==-1)
                     {
-                        // value does not exist, add forward reference to locctr + 1, since entry to change is in next byte
-                        insertForwardReference(operand,locctr+1);
-                        record+="0000";
+                        cout<<"Error: UNDEFINED SYMBOL " + operand + " line "<<dec<<linenumber;
+                        return 0;
                     }
                     else
                     {
@@ -327,11 +338,11 @@ int main(int argc, char const *argv[])
             // opcode is assembler directive
             else if(opcode=="WORD")
             {
+                lengthCurrentEntry=3;
                 // insert operand as 6 hex bit wide in textRecord
                 stringstream s;
                 s<<setw(6)<<setfill('0')<<hex<<operand;
                 record=s.str();
-                lengthCurrentEntry=3;
             }
             else if(opcode=="RESW")
             {
@@ -378,12 +389,11 @@ int main(int argc, char const *argv[])
                 cout<<"Error: INVALID OPCODE " + opcode + " line "<<dec<<linenumber;
                 return 0;
             }
-            locctr+=lengthCurrentEntry;
             if(breakOff==1)
             {
                 // break of textRecord in case of reserve statements
                 textRecords.push_back(currentTextRecord);
-                currentTextRecord = *new textRecord();
+                currentTextRecord = *new textRecord(locctrString);
             }
             else
             {
@@ -392,7 +402,7 @@ int main(int argc, char const *argv[])
                 {
                     // if space is limited create a new textRecord and save last one
                     textRecords.push_back(currentTextRecord);
-                    currentTextRecord = *new textRecord();
+                    currentTextRecord = *new textRecord(locctrString);
                     currentTextRecord.insertRecord(record,lengthCurrentEntry);
                 }
             }
@@ -401,6 +411,7 @@ int main(int argc, char const *argv[])
         linenumber++;
         // split next line, which was prefetched
         stringstream s(line);
+        getline(s,locctrString,' ');
         getline(s,label,' ');
         getline(s,opcode,' ');
         getline(s,operand,' ');
@@ -412,14 +423,9 @@ int main(int argc, char const *argv[])
     g.close();
     
     // open output file
-    f.open("output",ios::out);
+    f.open("outputTwoPass",ios::out);
     
     // Complete the header record with program size = locctr - startAdress in hex with 6 bit width
-    stringstream headerCompleter;
-    int sizeOfProgram = locctr-startaddress;
-    headerCompleter<<setw(6)<<setfill('0')<<hex<<sizeOfProgram;
-    headerRecord+=headerCompleter.str();
-    
     cout<<headerRecord<<endl;
     f<<headerRecord<<endl;
     
@@ -442,53 +448,49 @@ int main(int argc, char const *argv[])
     
     cout<<endRecord<<endl;
     f<<endRecord<<endl;
-    
+    f.close();
     // Successful Execution
     return 0;
 }
-
-
+int main(int argc, char const *argv[])
+{
+    if(passOne())
+        return passTwo();
+    return 0;
+}
 
 /*
-SAMPLE INPUT
+SAMPLE SOURCE
 =============
 COPY START 1000
-LDA ALPHA
-ADD BETA
-STA GAMMA
+ LDA ALPHA
+ ADD BETA
+ STA GAMMA
 EOF BYTE C'EOF'
 . this is a comment
 HEX BYTE X'F0'
 ALPHA WORD 1
-GAMMA RESW 1
 BETA WORD 2
-END COPY
+GAMMA RESW 4096
+END COPY 
 
-SAMPLE OPTAB
+INTERMEDIARY
 ============
-LDA 00
-STA 50
-ADD 6B
+1000 COPY START 1000
+1000  LDA ALPHA
+1003  STA GAMMA
+1006  ADD BETA
+1009 EOF BYTE C'EOF'
+100c HEX BYTE X'F0'
+100d ALPHA WORD 1
+1010 BETA WORD 2
+1013 GAMMA RESW 4096
 
-SAMPLE OUTPUT
-============
-1000: COPY START 1000
-1000:  LDA ALPHA
-1003:  ADD BETA
-1006:  STA GAMMA
-1009: EOF BYTE C'EOF'
-100c: HEX BYTE X'F0'
-100d: ALPHA WORD 1
-1010: GAMMA RESW 1
-1013: BETA WORD 2
 
-H^COPY^001000^000016
-T^001000^0d^000000^6B0000^500000^454f46^F0
-T^001001^02^100d
-T^00100d^03^000001
-T^001007^02^1010
-T^001004^02^1013
-T^001013^03^000002
+OUTPUT
+======
+H^COPY^001000^003013
+T^001000^13^00100d^501013^6B1010^454f46^F0^000001^000002
 E^001000
 */
 

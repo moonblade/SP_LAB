00b753feca4f99caee716347e89031c502aa9ae3
Nisham <moonblade168@gmail.com>
2016-04-24 19:06:08 +0530

    optimise machine code

--- 11-machineCodeGenerator/machineCodeGenerator.cpp
+++ 11-machineCodeGenerator/machineCodeGenerator.cpp
@@ -1,34 +1,17 @@
 #include "iostream"
 #include "fstream"
 #include "sstream"
-#include "list"
-#include <map>
-
+#include "map"
 using namespace std;
 int main(int argc, char const *argv[])
 {
-	map<string,string> op;
-	op["="]="MOV";
-	op["+"]="ADD";
-	op["-"]="SUB";
-	op["*"]="MUL";
-	op["/"]="DIV";
+	map<string,string> op={{"=","MOV"},{"+","ADD"},{"-","SUB"},{"*","MUL"},{"/","DIV"}};
 	ifstream f("input");
-	string line,element;
-	while(getline(f,line))
-	{
-		list<string> linel;
-		stringstream s(line);
-		string first;
-		getline(s,first,' ');
-		while(getline(s,element,' '))
-		{
-			string a;
-			getline(s,a,' ');
-			cout<<op[element]<<" "<<first<<", "<<a<<"\n";
-		}
-
-	}
+	string line,element,first,operatr;
+	stringstream s;
+	while(getline(f,line)&&(s=stringstream(line))&&getline(s,first,' '))
+		while(getline(s,element,' ')&&getline(s,operatr,' '))
+			cout<<op[element]<<" "<<first<<", "<<operatr<<"\n";
 	f.close();
 	return 0;
 }
